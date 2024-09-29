9602380991342e35ca0d752a9ec0ad5cd06ced1b
GiaBao <Giabao51003@gmail.com>
2024-09-29 11:43:35 +0700

    Update Project

--- app/src/main/java/com/example/appmanager/activity/MainActivity.java
+++ app/src/main/java/com/example/appmanager/activity/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //test git
+    //test git 2
 
     private void getEventClick() {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
--- app/src/main/java/com/example/appmanager/utils/Utils.java
+++ app/src/main/java/com/example/appmanager/utils/Utils.java
@@ -3,5 +3,5 @@ package com.example.appmanager.utils;
 import java.util.List;
 
 public class Utils {
-    public static String BASE_URL = "http://192.168.1.2/clothing/";
+    public static String BASE_URL = "http://192.168.56.1/clothing/";
 }
